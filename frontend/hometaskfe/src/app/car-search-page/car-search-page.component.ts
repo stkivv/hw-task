aae8f5439bf41544b4f2f81d8b42d49915d220aa
Stefan Kivvistik <stefankivv@gmail.com>
2024-09-30 21:32:01 +0300

    car search page

--- frontend/hometaskfe/src/app/car-search-page/car-search-page.component.ts
+++ frontend/hometaskfe/src/app/car-search-page/car-search-page.component.ts
@@ -1,12 +1,64 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car-search-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './car-search-page.component.html',
-  styleUrl: './car-search-page.component.scss'
+  styleUrl: './car-search-page.component.scss',
 })
 export class CarSearchPageComponent {
+  constructor(private http: HttpClient) {}
 
+  find = new FormControl('');
+  userId = new FormControl('');
+  carId = new FormControl('');
+  sort = new FormControl('id:asc');
+
+  data: any;
+
+  submitForm() {
+    let url = 'http://localhost:8080/';
+    let params = new HttpParams();
+
+    if (this.userId.value) {
+      this.doFetch(url + 'users/' + this.userId.value + '/cars', params);
+      return;
+    }
+
+    url += 'cars';
+
+    if (this.carId.value) {
+      this.doFetch(url + '/' + this.carId.value, params);
+      return;
+    }
+    if (this.find.value) {
+      params = params.append('find', this.find.value);
+    }
+    if (this.sort.value) {
+      params = params.append('sort', this.sort.value);
+    }
+    this.doFetch(url, params);
+  }
+
+  private doFetch(url: string, params: HttpParams) {
+    this.http.get(url, { params: params }).subscribe({
+      next: (response: any) => {
+        this.data = response;
+      },
+      error: (error: any) => {
+        this.data = 'Error fetching data!';
+        console.error(error);
+      },
+    });
+  }
 }
+
+bootstrapApplication(CarSearchPageComponent, {
+  providers: [provideHttpClient()],
+});
